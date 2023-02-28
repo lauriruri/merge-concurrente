fc998bab467d2fee29bf6ffa6cf605e29256486a
laura <lherre10@ucm.es>
2023-02-28 09:04:53 +0100

    solo falta que pare al final

--- practica.py
+++ practica.py
@@ -1,7 +1,3 @@
-from multiprocessing import Process
-from multiprocessing import current_process
-from multiprocessing import Value, Array
-from random import random
 
 '''
 Implementar un merge concurrente:
@@ -22,7 +18,7 @@ para poder elegir el mínimo tienen que haber producido un valor todos los consu
 
 luego el consumidor va cogiendo los numeros y ordenandolos de menor a mayor
 
-el prodeso acaba cuando algún productor esta  -1, ponemos un numero aleatorio de vueltas.
+el proceso acaba cuando algún productor esta  -1, ponemos un numero aleatorio de vueltas.
 
 fijamos n numero de vueltas y cada proceso crea 
 
@@ -46,32 +42,112 @@ Process(_,argr(val=values[i]))
 
 cada proceso tendrá que tener un semáforo
 
-un semaforo por cada proceso para indicar que el consumidor a consumido
+un semaforo por cada proceso para indicar que el consumidor a consumido (un semaforo por cada productor)
 un semaforo para decir que el consumidor ha consumido
+
 '''
 
 from multiprocessing import Process
 from multiprocessing import current_process
 from multiprocessing import Value, Array
 from multiprocessing import Semaphore
-from random import random
+from random import random,randint
 
 ##randint(1,30) me genera un numero entre 1 y 30 -> 14
 ##randint(14,30*2) etc.
+COTA = 100
+N=5 #numero de procesos
+#sem = Semaphore(0) #numero es numero de procesos que pueden entrar
+values=[]
+procesos=[]
+lleno=[]
+vacio=[]
+resultado=[]
 
-def llamar_proceso(i,cota):
+def llamar_proceso(proc_id):
+    value = 0
+    LIMITE = randint(20,30)
+    for i in range(0,LIMITE):
+        print("process",proc_id,"wait")
+        vacio[proc_id].acquire()
+        value = value + randint(0, COTA)
+        values[proc_id].value = value
+        print("process",proc_id,"produced value",values[proc_id].value)
+        lleno[proc_id].release()
+        print("process",proc_id,"release")
 
+    vacio[proc_id].acquire()
+    values[proc_id].value = -1
+    lleno[proc_id].release()
+
+def minimum_pos(l):
+    aux = l[0]
+    pos = 0
+    for i in range(1,len(l)):
+        if(l[i]<aux):
+            aux = l[i]
+            pos = i
+    return (aux, pos)
 
 
 def main():
-    N=5 #numero de procesos
-    sem = BoundedSemaphore(0) #numero es numero de procesos que pueden entrar
-    values=[]
-    procesos=[]
-    semaforos=[]
-    cota = 100
+
     for i in range(0,N):
-        values[i]=Value('i',-2) #inicializo los valores a -2
-        procesos.append(Process(target=llamar_proceso, args = (i,cota)))
+        values.append(Value('i',-2)) #inicializo los valores a -2
+        vacio.append(Semaphore(1))
+        lleno.append(Semaphore(0))
+        procesos.append(Process(target=llamar_proceso, args = (i,)))
+
+    for proceso in procesos:
+        proceso.start()
+
+#consumidor
+    local_values=[]
+    for i in range(0,N):       
+        print("consumer waiting for", i)
+
+        lleno[i].acquire()
+        local_values.append(values[i].value)
+        print ("este es el valor: ", values[i].value)
+        print("y así queda mi local_values ,", local_values)
+    print("initial values are:", local_values)
+    while True:
+        (minimo, posicion)=minimum_pos(local_values)
+        if (minimo == -1):
+            break
+        resultado.append(minimo)
+        print("consumer releasing", posicion)
+        vacio[posicion].release()
+        print("consumer waiting", posicion)
+        lleno[posicion].acquire()
+        local_values[posicion]=values[posicion].value
+        print("values are:", local_values)
+
+    
+    print(resultado)
 
 
+
+
+'''
+producer(i)
+loop
+    empty(i).wait()
+    producer
+    non_empty(i).signal()
+
+consumer //necesito que haya al menos un valor distinto de -1
+//si un proceso esta en -1 ya no produce mas!!!!
+for i
+    non_empty(i).wait()
+loop
+    i <- minimo
+    almacenamos
+    empty(i).signal() //despertamos al que ha producido al mínimo
+    non.empty(i).wait() 
+'''
+
+if __name__ == "__main__" :
+    main()
+    print("fin")
+    return
